eccbcee24cf044c25f7b1dbd9ac7755414a5d909
emre durgunlu <emredurgunlu@gmail.com>
2023-10-11 02:42:22 +0300

    Responsive-Image-Maps

---
+++ myScript.js
@@ -0,0 +1,35 @@
+function saladFunction() {
+  document.getElementById("header").innerHTML = "Salata - Salad";
+  document.getElementById("ingredientsTR").innerHTML =
+    "Havuç-Roka-Domates-Mısır-Marul karışımıyla elde edilen nefis salata. Zeytinyağı, nar ekşisi ve özel sosu ile lezzetlendirilmiş";
+  document.getElementById("ingredientsENG").innerHTML =
+    "Delicious salad made with a mixture of Carrot-Arugula-Tomato-Corn-Lettuce. Flavored with olive oil, pomegranate syrup and special sauce";
+}
+function potatoFunction() {
+  document.getElementById("header").innerHTML = "Patates - Chips";
+  document.getElementById("ingredientsTR").innerHTML =
+    "Kızarmış çıtır patates özel baharat karışımı ile tatlandırılmış";
+  document.getElementById("ingredientsENG").innerHTML =
+    "Fried crispy potatoes flavored with a special spice mixture";
+}
+function hotSouceFunction() {
+  document.getElementById("header").innerHTML = "Acı Sos - Hot Souce";
+  document.getElementById("ingredientsTR").innerHTML =
+    "Meksika biberi ketçap mayonez ve barbekü sosu karışımı";
+  document.getElementById("ingredientsENG").innerHTML =
+    "Mexican chili ketchup mayonnaise and barbecue sauce mix";
+}
+function freshSouceFunction() {
+  document.getElementById("header").innerHTML = "Yoğurtlu Sos - Fresh Souce";
+  document.getElementById("ingredientsTR").innerHTML =
+    "Yoğurt Mayonez ve Semiz otu karışımından elde edilen özel sos";
+  document.getElementById("ingredientsENG").innerHTML =
+    "Special sauce made from a mixture of Yoghurt, Mayonnaise and Purslane";
+}
+function burgerFunction() {
+  document.getElementById("header").innerHTML = "Burger";
+  document.getElementById("ingredientsTR").innerHTML =
+    "Hamburger eti, cheddar peyniri, domates, marul küçük boy susamlı sandviç ekmeği, salatalık turşusu, mayonez ve ketçaptan oluşan lezzet.";
+  document.getElementById("ingredientsENG").innerHTML =
+    "Delicacy consisting of hamburger meat, cheddar cheese, tomato, lettuce, small sesame sandwich bread, pickled cucumber, mayonnaise and ketchup.";
+}
